f1197211e9157f59a7237ae6f7655e10f06598af
AloneFancy <ruan.qing.xuan123@gmail.com>
2023-06-06 12:40:43 +0700

    Make Reqif Reader class

--- final.py
+++ final.py
@@ -1,106 +1,142 @@
 #!/usr/bin/env python
 
 import globals
-# from globals import * 
+
+# from globals import *
 from convert import *
-#import git_upload
-import json
+
+# import git_upload
+import json, os
 import config
+import configparser
 
-PATH = './'
 
-def extract_to_json(data):
+def check_os():
+    global PATH
+    if os.name == "nt":
+        PATH = "output\\"
+    elif os.name == "posix":
+        PATH = "output/"
+
+
+def write_to_json_file(data):
     try:
-        with open(PATH+"output/data.json", "w") as f:
-            json.dump(data, f,indent=4)
+        with open(PATH + "data.json", "w") as f:
+            json.dump(data, f, indent=4)
         f.close()
     except:
-        raise Exception("No permission to write file or check if output folder is created")
+        raise Exception(
+            "No permission to write file or check if output folder is created"
+        )
 
-def extract_to_rst(data):
+
+def write_to_rst_file(data):
     rst_file = RSTfile(data)
     try:
-        with open(PATH + "output/ECU_Req.rst", "w") as f:
+        with open(PATH + "ECU_Req.rst", "w") as f:
             f.flush()
             f.write(rst_file)
         f.close()
     except:
-        raise Exception("No permission to write file or check if output folder is created")
+        raise Exception(
+            "No permission to write file or check if output folder is created"
+        )
+    
 
-def read_json_file(path):
+def create_output_folder():
     """
-    Read Json data from path
+    Create output folder if not existed
     """
-    f = open(path,mode='r')
-    data = json.load(f)
-    f.close()
-    return data
+    path = "output"
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
 
-def extract_keys_values():
-    """
-    Variables guide:
-        list{'identifier':{obj:json{}}}
-        Output final : [list['identifier']]
-    """
-    list={}
-     
-    for iterate in globals.spec_objects:
-        obj={}
-        reference = globals.reqif_bundle.get_spec_object_by_ref(iterate.identifier)
-        obj["Attribute Type"]=globals.reqif_bundle.get_spec_object_type_by_ref(reference.spec_object_type).long_name
-        obj["Modified On"]=iterate.last_change
-        obj["Description"]=iterate.description if iterate.description else ""
-        obj["ReqIF.Text"]=""
-        for key in iterate.attribute_map:
-            def_ref = iterate.attribute_map.get(key).definition_ref
-            obj[return_key(globals.def_dictionary[def_ref])]=process_value(return_key(globals.def_dictionary[def_ref]),iterate.attribute_map.get(key).value)
-        obj.pop(None)
-        list[iterate.identifier]=obj
-    final = []
-    for iterate in globals.specifications:
-        for current_hierarchy_node in globals.hierachy(iterate):            
-            final.append(list[current_hierarchy_node.spec_object])
-    return final
 
-def process_value(key,value):
-    """
-    Resolve value for each key
-    """
-    enum_dictionary = globals.enum_dictionary
-    if key=='Identifier':
-        return int(value)
-    elif key=='Title':
-        return resolve_html_code(value)
-    elif key=='Safety Classification':
-        return enum_dictionary[value[0]]                      
-    elif key=='Status':
-        return enum_dictionary[value[0]]
-    elif key==None:
-        pass
-    return value
+class ReqIF_Reader:
+    def __init__(self):
+        """
+        Ready data to store in json file
+        """
+        temp_data = {}
+        temp_data["Module Name"] = globals.specifications[0].long_name
+        temp_data["Module Type"] = globals.spec_types[1].long_name
+        ListArtifactInfo = self.extract_keys_values()
+        temp_data["List Artifact Info"] = ListArtifactInfo
+        self.Data_in_json = temp_data
 
-def process_data():
-    """
-    Ready data to store in json file
-    """
-    temp_data = {}
-    temp_data["Module Name"] = globals.specifications[0].long_name
-    temp_data["Module Type"] = globals.spec_types[1].long_name
-    ListArtifactInfo = extract_keys_values() 
-    temp_data["List Artifact Info"] = ListArtifactInfo
-    return temp_data
+    def read_json_file(self, path):
+        """
+        Read Json data from path
+        """
+        f = open(path, mode="r")
+        data = json.load(f)
+        f.close()
+        return data
+
+    def extract_keys_values(self):
+        """
+        Variables guide:
+            list{'identifier':{obj:json{}}}
+            Output final : [list['identifier']]
+        """
+        list = {}
+
+        for iterate in globals.spec_objects:
+            obj = {}
+            reference = globals.reqif_bundle.get_spec_object_by_ref(iterate.identifier)
+            obj["Attribute Type"] = globals.reqif_bundle.get_spec_object_type_by_ref(
+                reference.spec_object_type
+            ).long_name
+            obj["Modified On"] = iterate.last_change
+            obj["Description"] = iterate.description if iterate.description else ""
+            obj["ReqIF.Text"] = ""
+            for key in iterate.attribute_map:
+                def_ref = iterate.attribute_map.get(key).definition_ref
+                obj[return_key(globals.def_dictionary[def_ref])] = self.process_value(
+                    return_key(globals.def_dictionary[def_ref]),
+                    iterate.attribute_map.get(key).value,
+                )
+            obj.pop(None)
+            list[iterate.identifier] = obj
+        final = []
+        for iterate in globals.specifications:
+            for current_hierarchy_node in globals.hierachy(iterate):
+                final.append(list[current_hierarchy_node.spec_object])
+        return final
+
+    def process_value(self, key, value):
+        """
+        Resolve value for each key
+        """
+        enum_dictionary = globals.enum_dictionary
+        if key == "Identifier":
+            return int(value)
+        elif key == "Title":
+            return resolve_html_code(value)
+        elif key == "Safety Classification":
+            return enum_dictionary[value[0]]
+        elif key == "Status":
+            return enum_dictionary[value[0]]
+        elif key == None:
+            pass
+        return value
+
+
 
 if __name__ == "__main__":
+    check_os()
+    create_output_folder()
     globals.init()
-    data_in_json = process_data()
-    extract_to_json(data_in_json)
-    extract_to_rst(data_in_json)
+    data_in_json = ReqIF_Reader()
     ### TASK 1
     config.configuration()
+    write_to_json_file(data_in_json.Data_in_json)
+    write_to_rst_file(data_in_json.Data_in_json)
     ### TASK 2
     """
     Read json file, return rst file
     """
-    data = read_json_file("data.json")
+    data = data_in_json.read_json_file(PATH + "data.json")
     rst_file_path = "ECU_Requirement.py"
     ### TASK 3
